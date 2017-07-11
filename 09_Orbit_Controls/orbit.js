a76e499217732432d845c38db388c8e5525c9450
Xtal Osc <xtalosc@gmx.com>
2017-07-11 12:48:57 -0500

    Fly controls

---
+++ 09_Orbit_Controls/orbit.js
@@ -0,0 +1,64 @@
+var example = (function(){
+  "use strict";
+
+  var scene = new THREE.Scene(),
+      renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer(),
+      light = new THREE.AmbientLight(0xFFFFFF),
+      camera,
+      controls,
+      box;
+
+      function initScene(){
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.getElementById("webgl-container").appendChild(renderer.domElement);
+
+        scene.add(light);
+
+        camera = new THREE.PerspectiveCamera(35, window.innerWidth/window.innerHeight, 1, 1000);
+
+        camera.position.z=100;
+        scene.add(camera);
+
+        box = new THREE.Mesh(
+          new THREE.BoxGeometry(20, 20, 20),
+          new THREE.MeshBasicMaterial({vertexColors: THREE.VertexColors})
+        );//end box
+
+        assignColorsToCube(box);
+        scene.add(box);
+
+        controls = new THREE.OrbitControls(camera);
+        controls.addEventListener('change', render);
+
+        render();
+      }//end initScene()
+
+      function assignColorsToCube(cube) {
+        var colors = [
+          new THREE.Color("rgb(255,0,0)"),
+          new THREE.Color("rgb(0,255,0)"),
+          new THREE.Color("rgb(0,0,255)"),
+          new THREE.Color("rgb(255,255,0)"),
+          new THREE.Color("rgb(0,255,255)"),
+          new THREE.Color("rgb(255,0,255)")
+        ];//end colors array
+
+        for (var i = 0; i < 12; i += 2) {
+          var color = colors[i / 2];
+          //each cube face is made up of 2 triangles and we want the same color for each
+          cube.geometry.faces[i].color = color;
+          cube.geometry.faces[i + 1].color = color;
+        }//end for loop
+      }//end assignColorsToCube()
+
+      function render() {
+        renderer.render(scene, camera);
+      }//end render()
+
+
+      window.onload = initScene;
+
+      return {
+        scene:scene
+      }
+})();//end example
--- 10_Fly_Controls/fly.js
+++ 10_Fly_Controls/fly.js
@@ -1,4 +1,3 @@
-console.log("orbit");
 var example = (function(){
   "use strict";
 
@@ -7,6 +6,8 @@ var example = (function(){
       light = new THREE.AmbientLight(0xFFFFFF),
       camera,
       controls,
+      clock = new THREE.Clock(),
+      plane,
       box;
 
       function initScene(){
@@ -17,43 +18,42 @@ var example = (function(){
 
         camera = new THREE.PerspectiveCamera(35, window.innerWidth/window.innerHeight, 1, 1000);
 
-        camera.position.z=100;
+        camera.position.z=300;
         scene.add(camera);
 
         box = new THREE.Mesh(
           new THREE.BoxGeometry(20, 20, 20),
-          new THREE.MeshBasicMaterial({vertexColors: THREE.VertexColors})
+          new THREE.MeshBasicMaterial({color:0xFF0000})
         );//end box
 
-        assignColorsToCube(box);
+        var texture = THREE.ImageUtils.loadTexture('content/grass.jpeg');
+        var planeMaterial = new THREE.MeshPhongMaterial({
+          map: texture,
+          side: THREE.DoubleSide
+        });//end planeMaterial
+
+        plane = new THREE.Mesh(new THREE.PlaneGeometry(800, 800), planeMaterial);
+        plane.rotation.x = 90 * (Math.PI / 180);
+        plane.position.y = -10;
+        plane.name ="plane";
+
+        scene.add(plane);
         scene.add(box);
 
-        controls = new THREE.OrbitControls(camera);
-        controls.addEventListener('change', render);
+        controls = new THREE.FlyControls(camera);
+        controls.movementSpeed = 100;
+        controls.domElement = document.getElementById("webgl-container");
+        controls.rollSpeed = Math.PI / 24;
 
         render();
       }//end initScene()
 
-      function assignColorsToCube(cube) {
-        var colors = [
-          new THREE.Color("rgb(255,0,0)"),
-          new THREE.Color("rgb(0,255,0)"),
-          new THREE.Color("rgb(0,0,255)"),
-          new THREE.Color("rgb(255,255,0)"),
-          new THREE.Color("rgb(0,255,255)"),
-          new THREE.Color("rgb(255,0,255)")
-        ];//end colors array
-
-        for (var i = 0; i < 12; i += 2) {
-          var color = colors[i / 2];
-          //each cube face is made up of 2 triangles and we want the same color for each
-          cube.geometry.faces[i].color = color;
-          cube.geometry.faces[i + 1].color = color;
-        }//end for loop
-      }//end assignColorsToCube()
-
       function render() {
+        var delta = clock.getDelta();
+        controls.update(delta);
+
         renderer.render(scene, camera);
+        requestAnimationFrame(render);
       }//end render()
 
 
